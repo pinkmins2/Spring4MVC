ce7182a61394bcf1d8d92476616b1fb13d45f579
pinkmins2 <pinkmins2@gmail.com>
2023-06-26 17:40:14 +0900

    DTO/VO 클래스에 lombok 적용

--- src/main/java/jina/hello/spring4/model/Member.java
+++ src/main/java/jina/hello/spring4/model/Member.java
@@ -1,79 +1,20 @@
 package jina.hello.spring4.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@ToString
 public class Member {
-    private  String mno;
-    private  String userid;
-    private  String passwd;
-    private  String name;
-    private  String email;
+    private String mno;
+    private String userid;
+    private String passwd;
+    private String name;
+    private String email;
     private String regdata;
 
-    public Member(String mno, String userid, String passwd, String name, String email, String regdata) {
-        this.mno = mno;
-        this.userid = userid;
-        this.passwd = passwd;
-        this.name = name;
-        this.email = email;
-        this.regdata = regdata;
-    }
-
-    public String getMno() {
-        return mno;
-    }
-
-    public void setMno(String mno) {
-        this.mno = mno;
-    }
-
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public String getPasswd() {
-        return passwd;
-    }
-
-    public void setPasswd(String passwd) {
-        this.passwd = passwd;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRegdata() {
-        return regdata;
-    }
-
-    public void setRegdata(String regdata) {
-        this.regdata = regdata;
-    }
-
-    @Override
-    public String  mtoString() {
-        return "Member{" +
-                "mno='" + mno + '\'' +
-                ", userid='" + userid + '\'' +
-                ", passwd='" + passwd + '\'' +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", regdata='" + regdata + '\'' +
-                '}';
-    }
 }
