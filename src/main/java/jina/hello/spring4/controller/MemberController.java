32ac5c27761a0ae03efb2af1d986d49a953e9bf2
pinkmins2 <pinkmins2@gmail.com>
2023-06-28 18:04:03 +0900

    회원가입 처리 - controller

--- src/main/java/jina/hello/spring4/controller/MemberController.java
+++ src/main/java/jina/hello/spring4/controller/MemberController.java
@@ -1,7 +1,10 @@
 package jina.hello.spring4.controller;
 
+import jina.hello.spring4.model.Member;
+import jina.hello.spring4.service.MemberService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,39 +15,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MemberController {
     private Logger logger = LogManager.getLogger(MemberController.class);
 
+    @Autowired
+    private MemberService msrv;
+
     @RequestMapping(value = "/member/join", method = RequestMethod.GET)
     public String join(Model m) {
-
         logger.info("member/join 호출!");
-
         return "member/join.tiles";
     }
     @RequestMapping(value = "/member/join", method = RequestMethod.POST)
-    public String joinok(Model m) {
-
+    public String joinok(Member m) {
         logger.info("member/joinok 호출!");
-
-        return "redirect:/member/login";
+        String viewName = "/member/fail";
+        if(msrv.saveMember(m))
+            viewName = "redirect:/member/login";    // 회원가입 처리
+        return viewName;
     }
     @RequestMapping(value = "/member/login", method = RequestMethod.GET)
     public String login(Model m) {
-
         logger.info("member/login 호출!");
-
         return "member/login.tiles";
     }
     @RequestMapping(value = "/member/login", method = RequestMethod.POST)
     public String loginok(Model m) {
-
         logger.info("member/loginok 호출!");
-
         return "redirect:/member/myinfo";
     }
     @RequestMapping("/member/myinfo")
     public String myinfo(Model m) {
-
         logger.info("member/myinfo 호출!");
-
         return "member/myinfo.tiles";
     }
 }
