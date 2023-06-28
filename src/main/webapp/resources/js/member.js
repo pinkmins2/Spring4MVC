a5394a2a2379ef2997af4159ef3ceea96e93431c
pinkmins2 <pinkmins2@gmail.com>
2023-06-28 14:58:03 +0900

    Member 관련 자바스크립트 작성

---
+++ src/main/webapp/resources/js/member.js
@@ -0,0 +1,36 @@
+/* join */
+let joinbtn = document.querySelector("#joinbtn");
+// jointbtn? : optinal chaining
+joinbtn?.addEventListener('click', () => {
+    //alert('joinbtn');
+    let frm = document.forms.joinfrm;
+
+    if(frm.userid.value === '') alert('아이디를 작성하세요!');
+    else if(frm.passwd.value === '') alert('비밀번호를 작성하세요!');
+    else if(frm.repwd.value === '') alert('비밀번호 확인을 작성하세요!');
+    else if(frm.repwd.value !== frm.passwd.value) alert('비밀번호가 일치하지 않습니다.!');
+    else if(frm.name.value === '') alert('이름을 작성하세요!');
+    else if(frm.email.value === '') alert('이메일을 작성하세요!');
+    else {
+        frm.method = 'post';
+        frm.action = '/member/join';
+        frm.submit();
+    }
+});
+
+
+
+/* login */
+let loginbtn = document.querySelector("#loginbtn");
+loginbtn?.addEventListener('click', () => {
+    //alert('loginbtn');
+    let frm = document.forms.loginfrm;
+
+    if(frm.userid.value === "") alert('아이디를 입력하세요!');
+    else if(frm.passwd.value === '') alert('비밀번호를 입력하세요!');
+    else {
+        frm.method = 'post';
+        frm.action = '/member/login';
+        frm.submit();
+    }
+});
