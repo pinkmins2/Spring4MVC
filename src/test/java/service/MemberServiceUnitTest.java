41c123f3a2be97f722514782b87f71998318cf1a
pinkmins2 <pinkmins2@gmail.com>
2023-06-29 15:17:39 +0900

    MemberService loginMember 단위 테스트

--- src/test/java/service/MemberServiceUnitTest.java
+++ src/test/java/service/MemberServiceUnitTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -27,6 +28,15 @@ public class MemberServiceUnitTest {
         Member m = new Member(null, "abc123a","987xyz","abc123","abc123@987xyz.co.kr",null);
         // assertEquals(테스트 메서드, 검증값)
         assertEquals(msrv.saveMember(m), true);
+    }
+
+    @Test
+    public  void loginMember() throws Exception {
+        Member m = new Member();
+        m.setUserid("abc123");
+        m.setPasswd("987xyz");
+        // assertEquals(테스트 메서드, 검증값)
+        assertEquals(msrv.loginMember(m), true);
     }
 
 }
