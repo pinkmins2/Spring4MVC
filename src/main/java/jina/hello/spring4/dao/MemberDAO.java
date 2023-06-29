cc1268417ca92d2388a0de61c274d9f7238e573f
pinkmins2 <pinkmins2@gmail.com>
2023-06-29 15:16:20 +0900

    MemberDAO loginMember 구현

--- src/main/java/jina/hello/spring4/dao/MemberDAO.java
+++ src/main/java/jina/hello/spring4/dao/MemberDAO.java
@@ -4,4 +4,5 @@ import jina.hello.spring4.model.Member;
 
 public interface MemberDAO {
     int insertMember(Member m);
+    Member loginMember(Member m);
 }
--- src/main/java/jina/hello/spring4/dao/MemberDAOImpl.java
+++ src/main/java/jina/hello/spring4/dao/MemberDAOImpl.java
@@ -4,12 +4,17 @@ import jina.hello.spring4.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository("mdao")
 public class MemberDAOImpl implements MemberDAO{
     // sql.properties에 작성한 sql 불러오기
     @Value("#{sql['insertMember']}") private String insertSQL;
+    @Value("#{sql['loginMember']}") private String loginSQL;
 
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -22,5 +27,28 @@ public class MemberDAOImpl implements MemberDAO{
 
         // 쿼리 실행: update(sql문, 매개변수)
         return jdbcTemplate.update(insertSQL, params);
+    }
+
+    public Member loginMember(Member m) {
+        // 매개변수 정의
+        Object[] params = new Object[] {
+                m.getUserid(), m.getPasswd()
+        };
+
+        // 로그인 매퍼 선언 - 콜백 함수
+        RowMapper<Member> mapper = new LoginMember();
+
+        // 쿼리 실행: queryForObject(sql문, 매개변수, 매퍼) - 단일값 반환
+        return jdbcTemplate.queryForObject(loginSQL, params, mapper);
+    }
+
+    private class LoginMember implements RowMapper<Member> {
+        @Override
+        public Member mapRow(ResultSet rs, int num) throws SQLException {
+            Member m = new Member();
+            m.setUserid(rs.getString(1));
+            m.setPasswd(rs.getString(2));
+            return m;
+        }
     }
 }
