cf037577fb7626a7d562747d27c98bd4e75bcaeb
pinkmins2 <pinkmins2@gmail.com>
2023-06-30 19:59:55 +0900

    board 관련 mapping 추가

---
+++ src/main/java/jina/hello/spring4/controller/BoardController.java
@@ -0,0 +1,38 @@
+package jina.hello.spring4.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    private Logger logger = LogManager.getLogger(BoardController.class);
+
+    public String list(Model m){
+
+        logger.info("board/list 호출!");
+
+        return "board/list.tiles";
+    }
+
+    @GetMapping("/write")
+    public String write(Model m){
+
+        logger.info("board/write 호출!");
+
+        return "board/write.tiles";
+    }
+
+    @GetMapping("/view")
+    public String view(Model m){
+
+        logger.info("board/view 호출!");
+
+        return "board/view.tiles";
+    }
+}
