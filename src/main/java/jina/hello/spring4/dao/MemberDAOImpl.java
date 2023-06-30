3dc3f0a585990822c3b7370b1a465b230695fec7
pinkmins2 <pinkmins2@gmail.com>
2023-06-30 19:44:35 +0900

    loginMember : queryForObject 대신 query로 변경

--- src/main/java/jina/hello/spring4/dao/MemberDAOImpl.java
+++ src/main/java/jina/hello/spring4/dao/MemberDAOImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 @Repository("mdao")
 public class MemberDAOImpl implements MemberDAO{
@@ -39,8 +40,14 @@ public class MemberDAOImpl implements MemberDAO{
         // 로그인 매퍼 선언 - 콜백 함수
         RowMapper<Member> mapper = new LoginMember();
 
-        // 쿼리 실행: queryForObject(sql문, 매개변수, 매퍼) - 단일값 반환
-        m = jdbcTemplate.queryForObject(loginSQL, params, mapper);
+        // 쿼리 실행 : queryForObject(sql문, 매개변수, 매퍼) - 단일값 반환
+        // 단, 결과가 없거나 둘 이상인 경우 예외발생! - 다루기 번거로움
+        // => JDK8 기능 중 Optional을 활용하거나
+        // => query(sql문, 매개변수, 매퍼) - 리스트 기반 다중값 반환
+        // m = jdbcTemplate.queryForObject(loginSQL, params, mapper);
+        List<Member> results =
+                jdbcTemplate.query(loginSQL, params, mapper);
+        m = results.isEmpty() ? null : results.get(0);
 
         return m;
     }
