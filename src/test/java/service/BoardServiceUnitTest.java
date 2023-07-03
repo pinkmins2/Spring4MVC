21043cbdce8e12dfec8375570db55aa188b868ea
pinkmins2 <pinkmins2@gmail.com>
2023-07-03 14:53:05 +0900

    BoardService : saveBoard 단위 테스트

--- src/test/java/service/BoardServiceUnitTest.java
+++ src/test/java/service/BoardServiceUnitTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -39,6 +40,13 @@ public class BoardServiceUnitTest {
 
         assertNotNull(result);
         System.out.println(result);
+    }
+
+    @Test
+    @Transactional
+    public  void saveBoard() throws Exception {
+        Board bd = new Board(null,"제목t","abc123","null",null,"내용t");
+        assertEquals(bsrv.saveBoard(bd),true);
     }
 
 }
