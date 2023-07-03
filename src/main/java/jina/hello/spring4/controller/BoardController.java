e80e93fd1bade56fe5beb37bbf01978c36f75755
pinkmins2 <pinkmins2@gmail.com>
2023-07-03 09:40:53 +0900

    BoardController : list 구현

--- src/main/java/jina/hello/spring4/controller/BoardController.java
+++ src/main/java/jina/hello/spring4/controller/BoardController.java
@@ -1,7 +1,9 @@
 package jina.hello.spring4.controller;
 
+import jina.hello.spring4.service.BoardService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,28 +13,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+
+    @Autowired
+    BoardService bsrv;
+
     private Logger logger = LogManager.getLogger(BoardController.class);
 
-    public String list(Model m){
-
-        logger.info("board/list 호출!");
+    @GetMapping("/list")
+    public String list(Model m, int cpg) {
+        logger.info("board/list 호출!!");
+        m.addAttribute("boards", bsrv.readBoard(cpg));
+        //m.addAttribute("psnum", bsrv.readBoard(cpg)/10);    // 페이지 네이션 시작 번호
+        //m.addAttribute("allpg", bsrv.readBoard(cpg)/10);    // 총 페이지 수
 
         return "board/list.tiles";
     }
 
     @GetMapping("/write")
-    public String write(Model m){
+    public String write() {
 
-        logger.info("board/write 호출!");
 
         return "board/write.tiles";
     }
 
     @GetMapping("/view")
-    public String view(Model m){
+    public String view() {
 
-        logger.info("board/view 호출!");
 
         return "board/view.tiles";
     }
+
+
 }
